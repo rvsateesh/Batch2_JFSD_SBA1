e28268acb08b50de7701299eda5f9f89b25514dd
rvsateesh <70032097+rvsateesh@users.noreply.github.com>
2020-08-23 10:18:15 +0530

    Add files via upload

---
+++ CoronaKitApplication/src/main/sba1db.sql
@@ -0,0 +1,37 @@
+DROP DATABASE sba1DB;
+
+CREATE DATABASE sba1DB;
+
+USE sba1DB;
+
+CREATE TABLE products(
+	pid int AUTO_INCREMENT primary key,
+	pname varchar(20) not null,
+	pcost decimal not null,
+	pdesc varchar(40)
+);
+
+INSERT INTO products VALUES
+(1, "Face Mask", "30.0", "Reusable Cotton Face Mask"),
+(2, "Sanitizer", "100.0", "Alchohol based sanitizer"),
+(3, "Remidesivir", "5000.0", "Immunity boosting medicine");
+
+CREATE TABLE orderdetails(
+	pid int not null,
+    pname varchar(20) not null,
+    pcost decimal not null,
+    pquantity int not null
+    );
+    
+INSERT INTO orderdetails VALUES
+(1, "Face Mask", "30.0", "5"),
+(2, "Sanitizer", "100.0", "4"),
+(3, "Remidesivir", "5000.0", "0");
+
+DELIMITER #
+CREATE TRIGGER prodinstrigger after INSERT ON products 
+for Each ROW 
+begin 
+INSERT INTO orderdetails (pid,pname,pcost,pquantity) VALUES (new.pid, new.pname, new.pcost, '0'); 
+end;
+#
